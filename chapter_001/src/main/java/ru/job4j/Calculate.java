f1d55645236794fcec259ce0087f8212bd7acd51
druzhba <ant341@yandex.ru>
2017-06-17 17:53:02 +0300

    add package-info.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вывода строки "Hello world" в консоль
+ * @author listratov
+ * @since 17.06.2017
+ * version 1
+ */
+
 public class Calculate {
+	/**
+	* Конструктор, вывод строки в консоль.
+	* @param arg - arg.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task
+ * @author Anton Listratov (mailto:ant341@yandex.ru)
+ * @version $ld$
+ * @since 0.1
+ */
+ package ru.job4j;
