e144e0ab5728ea611771c58d9f6e2ba7a5191898
druzhba <ant341@yandex.ru>
2017-07-08 15:31:17 +0300

    3.3 Вычисление площади треугольника[#9461].

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -35,11 +35,18 @@ public class Triangle {
  * @return площадь треугольника
  */
 	public double area() {
-		double result = Math.abs(((this.a.getX() - this.c.getX()) * (this.b.getY() - this.c.getY())) - ((this.b.getX() - this.c.getX()) * (this.a.getY() - this.c.getY())));
-		if (result != 0) {
-			return result / 2;
+		double ab = Math.sqrt(Math.pow(this.b.getX() - this.a.getX(), 2) + Math.pow(this.b.getY() - this.a.getY(), 2));
+		double ca = Math.sqrt(Math.pow(this.c.getX() - this.a.getX(), 2) + Math.pow(this.c.getY() - this.a.getY(), 2));
+		double bc = Math.sqrt(Math.pow(this.c.getX() - this.b.getX(), 2) + Math.pow(this.c.getY() - this.b.getY(), 2));
+		if ((ab + bc <= ca) || (ab + ca <= bc) || (ca + bc <= ab)) {
+			return 0;
 		} else {
+		double result = Math.abs(((this.a.getX() - this.c.getX()) * (this.b.getY() - this.c.getY())) - ((this.b.getX() - this.c.getX()) * (this.a.getY() - this.c.getY())));
+		if ((result == 0)) {
 			return result;
+		} else {
+			return result / 2;
+		}
 		}
 	}
 }
